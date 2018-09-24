aaa139cc91aec10b2f137d74f13ac3c9a2a882fd
Ebad Hashmi <ibad.hashmi@yahoo.com>
2018-09-24 17:57:16 +0500

    v0.1.1

--- projects/ngx-meter/src/lib/ngx-meter.component.ts
+++ projects/ngx-meter/src/lib/ngx-meter.component.ts
@@ -13,6 +13,10 @@ export class NgxMeterComponent implements OnInit, OnChanges {
   @Input() width = 0;
   @Input() score = 0;
   @Input() colors = ['red', 'orange', 'yellow', '#b5e416', 'green'];
+  @Input() gradient = null;
+  @Input() innerRadius = 30;
+  @Input() linearGradient = null;
+  @Input() radialGradiant = null;
 
   context: any;
   canvas: any;
@@ -62,7 +66,7 @@ export class NgxMeterComponent implements OnInit, OnChanges {
       this.range = 5;
     }
 
-    this.drawGauge(this.height, this.width, this.healthInRadian, this.range, this.colors);
+    this.drawGauge(this.height, this.width, this.healthInRadian, this.range, this.colors, this.gradient);
   }
 
   public drawNeedle(x, y, radius, radianAngle, color) {
@@ -81,13 +85,36 @@ export class NgxMeterComponent implements OnInit, OnChanges {
     this.context.fill();
   }
 
-  public drawSlice(radius, innerRadius, start, end, color, counterClockWise) {
+  public drawSlice(radius, innerRadius, start, end, color, gradientColor, counterClockWise) {
     this.context.fillStyle = color;
     this.context.beginPath();
     this.context.moveTo(this.center.x, this.center.y);
     this.context.arc(this.center.x, this.center.y, radius, start, end, counterClockWise);
     this.context.arc(this.center.x, this.center.y, innerRadius, end, start, !counterClockWise);
     this.context.closePath();
+    if (gradientColor) {
+      let gradient;
+      if (this.linearGradient) {
+        gradient = this.context.createLinearGradient(
+          this.linearGradient[0],
+          this.linearGradient[1],
+          this.linearGradient[2],
+          this.linearGradient[3]
+        );
+      } else {
+        gradient = this.context.createRadialGradient(
+          this.radialGradiant[0],
+          this.radialGradiant[1],
+          this.radialGradiant[2],
+          this.radialGradiant[3],
+          this.radialGradiant[4],
+          this.radialGradiant[5],
+        );
+      }
+      gradient.addColorStop(1, color);
+      gradient.addColorStop(0, gradientColor);
+      this.context.fillStyle = gradient;
+    }
     this.context.fill();
     this.context.strokeStyle = 'white';
     this.context.stroke();
@@ -101,7 +128,7 @@ export class NgxMeterComponent implements OnInit, OnChanges {
     this.context.stroke();
   }
 
-  public drawGauge(width, height, needlePointer, _range, colors) {
+  public drawGauge(width, height, needlePointer, _range, colors, gradientColors) {
     this.canvas = document.getElementById('canvas');
     this.context = this.canvas.getContext('2d');
 
@@ -115,11 +142,11 @@ export class NgxMeterComponent implements OnInit, OnChanges {
     const range: any = fullCircle - diff;
     const sliceDiv = 1;
 
-    this.drawSlice((this.canvas.width / 2) - 20, 30, (fullCircle - (1 * part)), fullCircle - ((1 - 1) * part), colors[0], false);
-    this.drawSlice((this.canvas.width / 2) - 20, 30, (fullCircle - (2 * part)), fullCircle - ((2 - 1) * part), colors[1], false);
-    this.drawSlice((this.canvas.width / 2) - 20, 30, (fullCircle - (3 * part)), fullCircle - ((3 - 1) * part), colors[2], false);
-    this.drawSlice((this.canvas.width / 2) - 20, 30, (fullCircle - (4 * part)), fullCircle - ((4 - 1) * part), colors[3], false);
-    this.drawSlice((this.canvas.width / 2) - 20, 30, (fullCircle - (5 * part)), fullCircle - ((5 - 1) * part), colors[4], false);
+    this.drawSlice((this.canvas.width / 2) - 20, this.innerRadius, (fullCircle - (1 * part)), fullCircle - ((1 - 1) * part), colors[0], gradientColors ? gradientColors[0] : gradientColors, false);
+    this.drawSlice((this.canvas.width / 2) - 20, this.innerRadius, (fullCircle - (2 * part)), fullCircle - ((2 - 1) * part), colors[1], gradientColors ? gradientColors[1] : gradientColors, false);
+    this.drawSlice((this.canvas.width / 2) - 20, this.innerRadius, (fullCircle - (3 * part)), fullCircle - ((3 - 1) * part), colors[2], gradientColors ? gradientColors[2] : gradientColors, false);
+    this.drawSlice((this.canvas.width / 2) - 20, this.innerRadius, (fullCircle - (4 * part)), fullCircle - ((4 - 1) * part), colors[3], gradientColors ? gradientColors[3] : gradientColors, false);
+    this.drawSlice((this.canvas.width / 2) - 20, this.innerRadius, (fullCircle - (5 * part)), fullCircle - ((5 - 1) * part), colors[4], gradientColors ? gradientColors[4] : gradientColors, false);
     //drawSlice(30, Math.PI, 0, 'white');
 
     this.drawArc((this.canvas.width / 2) - 10, 16, Math.PI, 0, '#4b4b4b', false);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent implements OnInit {
   width = 300;
   score = 0;
   colors = ['red', 'orange', 'yellow', '#b5e416', 'green'];
+  gradient = ['yellow', '#f9c970', '#ffffbe', '#b5e41691', 'lightgreen'];
+  innerRadius = 40;
+  radialGradiant = [150, 0, 0, 150, 150, 150];
 
   constructor () {
 
